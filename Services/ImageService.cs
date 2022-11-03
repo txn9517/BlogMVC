7b1f1fed78714b14f201c4527381f4848ed0c9bf
Tom Nguyen <tndallas9517@gmail.com>
2022-11-03 18:20:18 -0500

    221103 Models scaffolding for db, Image upload prep

---
+++ Services/ImageService.cs
@@ -0,0 +1,49 @@
+﻿using BlogMVC.Services.Interfaces;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BlogMVC.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly string defaultImage = "/img/DefaultBlogImage.png";
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            if (fileData == null)
+            {
+                return defaultImage;
+            }
+
+            try
+            {
+                string imageBase64Data = Convert.ToBase64String(fileData);
+
+                string imageSrcString = string.Format($"data:{extension};base64, {imageBase64Data}");
+
+                return imageSrcString;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            try
+            {
+                using MemoryStream memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteFile = memoryStream.ToArray();
+                memoryStream.Close();
+
+                return byteFile;
+
+            } catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogMVC.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
