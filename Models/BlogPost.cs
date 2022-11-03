5d6f46e53351ba07adb027cd76d735e1bb38e652
Tom Nguyen <tndallas9517@gmail.com>
2022-11-03 18:06:06 -0500

    221103 Models scaffolding for db, Image prep

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -1,7 +1,54 @@
-﻿namespace BlogMVC.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogMVC.Models
 {
     // the parts of a blog post
     public class BlogPost
     {
+        // primary key
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 2)]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime LastUpdated { get; set; }
+
+        // foreign key
+        public int CategoryId { get; set; }
+
+        public string? Slug { get; set; }
+
+        public string? Abstract { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        [DisplayName("Published")]
+        public bool IsPublished { get; set; }
+
+        public byte[]? ImageData { get; set; }
+
+        public string? ImageType { get; set; }
+
+        [NotMapped]
+        public IFormFile? BlogPostImage { get; set; }
+
+        // add relationship to Category model
+        public virtual Category? Category { get; set; }
+
+        // add relationship to Comment model
+        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
+
+        // add relationship to Tag model
+        public virtual ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
     }
 }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,7 +1,18 @@
-﻿namespace BlogMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogMVC.Models
 {
     // blueprint for a tag
     public class Tag
     {
+        // primary key
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long", MinimumLength = 2)]
+        public string? Name { get; set; }
+
+        // navigation properties
+        public virtual ICollection<BlogPost> BlogPosts { get; set; } = new HashSet<BlogPost>();
     }
 }
